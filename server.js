2e8f3fb47a5605c32c54f1afe44e326b8e96c97f
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-06-08 20:46:19 -0300

    Express para page not found

--- server.js
+++ server.js
@@ -6,22 +6,19 @@ var express = require('express'),
   bodyParser = require('body-parser');
   
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://adotapets:admin@cluster0-shard-00-00-h6bbx.mongodb.net:27017,cluster0-shard-00-01-h6bbx.mongodb.net:27017,cluster0-shard-00-02-h6bbx.mongodb.net:27017/estudo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
-// mongoose.connect('mongodb://adotapets:admin@cluster0-shard-00-00-h6bbx.mongodb.net:27017/estudo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
-mongoose.connect('mongodb://localhost:27017/estudo', null, () => {
-	console.log("Error a", arguments)
-})
-
+mongoose.connect('mongodb://adotapets:admin@cluster0-shard-00-00-h6bbx.mongodb.net:27017,cluster0-shard-00-01-h6bbx.mongodb.net:27017,cluster0-shard-00-02-h6bbx.mongodb.net:27017/estudo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+// mongoose.connect('mongodb://localhost:27017/estudo')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res) {
+	res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
 
-
 app.listen(port);
-
 
 console.log('todo list RESTful API server started on: ' + port);
