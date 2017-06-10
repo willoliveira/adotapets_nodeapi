a0df08390ced50619773d16a76fd210c6c7db120
Rafael Odassi <rafaelodassi@hotmail.com>
2017-06-10 20:16:29 -0300

    Add index_search.js

---
+++ index_search.js
@@ -0,0 +1,18 @@
+var MongoDB = require('./src/class/MongoDB')();
+var Config = require('./src/class/Config');
+var database = Config.database;
+var db;
+
+db = MongoDB.connect(database);
+
+db.on('error', (error) => {
+	throw error
+});
+
+db.on('disconnected', () => {
+	console.log('Database connection closed')
+});
+
+db.on('connected', () => {
+	console.log('Database connected')
+});
