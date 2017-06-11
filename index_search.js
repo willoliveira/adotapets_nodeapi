0fc4f4c018a85a4657887a34835b3553c0d6cbf0
Rafael Odassi <rafaelodassi@hotmail.com>
2017-06-10 21:10:36 -0300

    Updates search

--- index_search.js
+++ index_search.js
@@ -1,6 +1,7 @@
 var MongoDB = require('./src/class/MongoDB')();
 var Config = require('./src/class/Config');
 var database = Config.database;
+var mongoose = require('mongoose');
 var db;
 
 db = MongoDB.connect(database);
@@ -15,4 +16,40 @@ db.on('disconnected', () => {
 
 db.on('connected', () => {
 	console.log('Database connected')
+
+	var Schema = mongoose.Schema;
+
+	var usersSchema = new Schema({
+		description: String,
+		email: String,
+		name: String,
+		location: { lat: String, lon: String },
+		picture: String,
+		pets: {}		
+	});
+
+	var User = mongoose.model('Users', usersSchema);
+
+	User.findOne({ 'name': 'Willian' }, function (err, user) {
+		if (err) return handleError(err);
+
+		console.log('%s', user);
+	})
+
+	/*var usersResult = db.users.find(
+		{
+			location: {
+				$nearSphere: {
+					$geometry: {
+						type : "Point",
+						coordinates : [ -73.9667, 40.78 ]
+					},
+					$minDistance: 1000,
+					$maxDistance: 5000
+				}
+			}
+		}
+	)*/
+
+	//console.log(usersResult);
 });
