06b37bb09dc60f0a8d82c44078fa22e4afb9b7c4
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-07-03 21:54:48 -0300

    Get room by user and participant

--- src/module/room/room.ctrl.js
+++ src/module/room/room.ctrl.js
@@ -14,6 +14,35 @@ class RoomController extends BaseController {
 			.get(this.get.bind(this))
 			.put(this.put.bind(this))
 			.delete(this.delete.bind(this));
+
+		this.bind('/room/user/:userId')
+			.get(this.getByUser.bind(this));
+		
+		this.bind('/room/user/:userId/participant/:participantId')
+			.get(this.getByUser.bind(this));
+	}
+
+	getByUser(req, res) {
+		let querry = { participants: "" };
+		let arrayUsers = [req.params.userId]
+
+		if (req.params.participantId) {
+			arrayUsers.push(req.params.participantId);
+			querry.participants = { $all: arrayUsers };
+		} else {
+			querry.participants = { $in: arrayUsers };
+		}
+		console.log(querry)
+		this.entity
+			.find(querry)
+			.exec((err, rooms) => {
+				if (err) {
+					res.status(500).send(err);
+				}
+				res.status(200).json({
+					content:  rooms
+				});
+			});
 	}
 }
 
